c5ed1bac21afe7438dfb9b4ea0404f555da7f66b
Michael Kenney <mkenney@webbedlam.com>
2018-08-08 23:50:05 -0600

    more test updates

--- example_basic_test.go
+++ example_basic_test.go
@@ -61,10 +61,10 @@ func Example_basic() {
 	}).Panic("It's over 9000!")
 
 	// Output:
-	// level="debug" msg="Started observing beach" animal="walrus" number="8" caller="example_basic_test.go:41 github.com/bdlm/log_test.Example_basic"
-	// level="info" msg="A group of walrus emerges from the ocean" animal="walrus" size="10" caller="example_basic_test.go:46 github.com/bdlm/log_test.Example_basic"
-	// level="warning" msg="The group's number increased tremendously!" number="122" omg="true" caller="example_basic_test.go:51 github.com/bdlm/log_test.Example_basic"
-	// level="debug" msg="Temperature changes" temperature="-4" caller="example_basic_test.go:55 github.com/bdlm/log_test.Example_basic"
-	// level="panic" msg="It's over 9000!" animal="orca" size="9009" caller="example_basic_test.go:60 github.com/bdlm/log_test.Example_basic"
-	// level="error" msg="The ice breaks!" err_animal="orca" err_level="panic" err_message="It's over 9000!" err_size="9009" number="100" omg="true" caller="example_basic_test.go:34 github.com/bdlm/log_test.Example_basic.func1"
+	// level="debug" msg="Started observing beach" animal="walrus" number="8" caller="example_basic_test.go:42 github.com/bdlm/log_test.Example_basic"
+	// level="info" msg="A group of walrus emerges from the ocean" animal="walrus" size="10" caller="example_basic_test.go:47 github.com/bdlm/log_test.Example_basic"
+	// level="warning" msg="The group's number increased tremendously!" number="122" omg="true" caller="example_basic_test.go:52 github.com/bdlm/log_test.Example_basic"
+	// level="debug" msg="Temperature changes" temperature="-4" caller="example_basic_test.go:56 github.com/bdlm/log_test.Example_basic"
+	// level="panic" msg="It's over 9000!" animal="orca" size="9009" caller="example_basic_test.go:61 github.com/bdlm/log_test.Example_basic"
+	// level="error" msg="The ice breaks!" err_animal="orca" err_level="panic" err_message="It's over 9000!" err_size="9009" number="100" omg="true" caller="example_basic_test.go:35 github.com/bdlm/log_test.Example_basic.func1"
 }
