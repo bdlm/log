d49959323691c5a3d13b8be98a77ee1420c9ad4e
Michael Kenney <mkenney@webbedlam.com>
2018-10-24 09:44:24 -0600

    fix upstream build dependency (#15)
    * fix upstream build dependency
    
    * remove go v1.8 support

--- .travis.sh
+++ .travis.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 set -e
 
-go get -v github.com/golang/lint/golint
+exit_code=0
+
+go get -u golang.org/x/lint/golint
 [ "0" = "$?" ] || exit 1
 
 go get -u github.com/golang/dep/cmd/dep
@@ -10,9 +12,21 @@ go get -u github.com/golang/dep/cmd/dep
 dep ensure
 [ "0" = "$?" ] || exit 3
 
+for dir in $(go list ./... | grep -v vendor); do
+    echo "golint $dir"
+    result=$(golint $dir)
+    if [ "" != "$result" ]; then
+        echo $result
+        exit_code=5
+    fi
+    if [ "0" != "$exit_code" ]; then
+        exit $exit_code
+    fi
+done
+
 rm -f coverage.txt
 for dir in $(go list ./... | grep -v vendor); do
-    go test -timeout 20s -coverprofile=profile.out $dir
+    GOCACHE=off go test -timeout 20s -coverprofile=profile.out $dir
     exit_code=$?
     if [ "0" != "$exit_code" ]; then
         exit $exit_code
@@ -20,16 +34,6 @@ for dir in $(go list ./... | grep -v vendor); do
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
-    fi
-done
-
-exit_code=0
-for dir in $(go list ./... | grep -v vendor); do
-    echo "golint $dir"
-    result=$(golint $dir)
-    if [ "" != "$result" ]; then
-        echo $result
-        exit_code=5
     fi
 done
 
